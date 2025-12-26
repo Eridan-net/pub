7cd31350e5f1eef1a2b296b6bfc67ec15fb62bc0
Eridan-net <robert@eridan.net>
2025-12-26 17:52:32 +0100

    Last Sync: 2025-12-26 17:52 (Mobile)

--- .lib/load.js
+++ .lib/load.js
@@ -1,31 +1,21 @@
 
-function loadText(src, sef, params) {
-	fetch(src)
+function loadText(spec) {
+	fetch(spec.src)
 		.then(f => f.text())
-		.then(t => transformText(t, sef, params))
-		.then(doc => replace(doc))
+		.then(t => transformText(t, spec))
+		.then(doc => replace(spec.doc))
 	}
-function transformText(doc, sef, params) {
-	return fetch(sef, {mode: 'cors'})
+function transformText(doc, spec) {
+	return fetch(spec.params.lib+'/'+spec.sef, { mode: 'cors' })
 		.then(f => f.text())
 		.then(t => SaxonJS.transform( 
 			{ sourceText: doc, stylesheetText: t
-			, stylesheetParams: params
+			, stylesheetParams: spec.params
 			, destination: 'serialized' }
 			, 'sync')
 			)
 		.then(r => r.principalResult)
 	}
-/*
-function load(doc, sef, params) {
-	let r= SaxonJS.transform(
-		{ sourceLocation: doc, stylesheetLocation: sef
-		, stylesheetParams: params }
-		, destination: 'serialized' }
-		, 'sync')
-	replace(r.principalResult)
-	}
-	*/
 function replace(doc) {
 	document.open("text/html", "replace");
 	document.write(doc);
